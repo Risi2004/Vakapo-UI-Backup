ad41d820ade285cd78d5d96624dddaf9d225de3f
Risi2004 <risikesanjegatheesan@gmail.com>
2025-09-01 13:55:58 +0530

    Updated Vaccination History Page

---
+++ Patient Page/VaccinationHistoryPage/VaccinationHistoryPage.js
@@ -0,0 +1,43 @@
+// announcement
+
+const cancelAnnouncement = document.querySelector(".announcement__cancel")
+const announcement = document.querySelector(".announcement")
+
+cancelAnnouncement.addEventListener("click" , function(){
+    announcement.style.display = "none"
+})
+
+// menu bar
+
+const menuBar = document.querySelector(".navbar__section4")
+const cancelMenubar = document.querySelector(".responsive__navbar__section1 p")
+const responsiveNavbar = document.querySelector(".responsive__navbar")
+const navbarLink = document.querySelectorAll(".responsive__navbar__section1 a")
+
+menuBar.addEventListener("click" , function(){
+    responsiveNavbar.style.right = "0%"
+})
+
+cancelMenubar.addEventListener("click" , function(){
+    responsiveNavbar.style.right = "-80%"
+})
+
+navbarLink.forEach(function(link){
+    link.addEventListener("click" , function(){
+        responsiveNavbar.style.right = "-80%"
+    })
+})
+
+//profile 
+
+const profile = document.querySelector(".navbar__section3")
+const cancelProfile = document.querySelector(".profile__close")
+const profileBox = document.querySelector(".profile")
+
+profile.addEventListener("click" , function(){
+    profileBox.style.right = "1%"
+})
+
+cancelProfile.addEventListener("click" , function(){
+    profileBox.style.right = "-20%"
+})
