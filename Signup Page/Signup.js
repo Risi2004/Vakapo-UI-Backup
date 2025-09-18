3b6573e68d1ee5343c891fd4080c4180914a1bcd
Risi2004 <risikesanjegatheesan@gmail.com>
2025-09-18 12:40:03 +0530

    Signup Gurdian Name Added

--- Signup Page/Signup.js
+++ Signup Page/Signup.js
@@ -11,10 +11,36 @@ role.addEventListener('change', function () {
     }
 })
 
+// patient age calculation
+
+const dobInput = document.getElementById('dob');
+const ageInput = document.getElementById('age');
+const gnameInput = document.querySelector('.signup__input__gname');
+
+dobInput.addEventListener('change', function () {
+    const dob = new Date(dobInput.value);
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+    ageInput.value = age;
+
+    if (age < 0) {
+        ageInput.value = '0';
+    }
+
+    if (age < 1) {
+        gnameInput.style.display = 'block';
+    }else{
+        gnameInput.style.display = 'none';
+    }
+});
+
 // hospital
 
 const hospitalSignup = document.querySelector('.signup__hospital');
-
 role.addEventListener('change', function () {
     if (role.value === 'Hospital') {
         hospitalSignup.style.display = 'block';
